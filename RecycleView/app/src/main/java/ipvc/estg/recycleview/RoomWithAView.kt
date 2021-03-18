4438a1b7ffb4bf79057a3e032ca8d96dcb17cf1b
Duarte-Dia <61877146+Duarte-Dia@users.noreply.github.com>
2021-03-18 19:07:28 +0000

    start of crud project

---
+++ RecycleView/app/src/main/java/ipvc/estg/recycleview/RoomWithAView.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.recycleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomWithAView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_with_a_view)
+    }
+}
