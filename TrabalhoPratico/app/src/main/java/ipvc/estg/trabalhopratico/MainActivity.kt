b9975b1d71772bdb62d7c26c4fefcf58c99d1f0b
Duarte-Dia <61877146+Duarte-Dia@users.noreply.github.com>
2021-04-08 20:00:57 +0100

    onClick added

--- TrabalhoPratico/app/src/main/java/ipvc/estg/trabalhopratico/MainActivity.kt
+++ TrabalhoPratico/app/src/main/java/ipvc/estg/trabalhopratico/MainActivity.kt
@@ -3,29 +3,31 @@ package ipvc.estg.trabalhopratico
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
-    fun onClickNotas(){
+    fun onClickNotas(view: View) {
         var intent = Intent(this,NoteListActivity::class.java)
         startActivity(intent)
     }
 
-    fun onClickLogin(){
+    fun onClickLogin(view: View){
         var intent = Intent(this,NoteListActivity::class.java)
         startActivity(intent)
     }
 
-    fun onClickMap(){
+    fun onClickMap(view: View){
         var intent = Intent(this,NoteListActivity::class.java)
         startActivity(intent)
     }
 
-    fun onClickDefinitions(){
+    fun onClickDefinitions(view: View){
         var intent = Intent(this,NoteListActivity::class.java)
         startActivity(intent)
     }
