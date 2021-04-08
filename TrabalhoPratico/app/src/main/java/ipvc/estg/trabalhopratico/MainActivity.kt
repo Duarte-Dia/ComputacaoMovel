a91a8f225c918c76a1f420303dafb6c42e49c133
Duarte-Dia <61877146+Duarte-Dia@users.noreply.github.com>
2021-04-08 19:52:15 +0100

    adicao de ultimas atividades

--- TrabalhoPratico/app/src/main/java/ipvc/estg/trabalhopratico/MainActivity.kt
+++ TrabalhoPratico/app/src/main/java/ipvc/estg/trabalhopratico/MainActivity.kt
@@ -1,5 +1,6 @@
 package ipvc.estg.trabalhopratico
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickNotas(){
+        var intent = Intent(this,NoteListActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickLogin(){
+        var intent = Intent(this,NoteListActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickMap(){
+        var intent = Intent(this,NoteListActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickDefinitions(){
+        var intent = Intent(this,NoteListActivity::class.java)
+        startActivity(intent)
     }
 }
